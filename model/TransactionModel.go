36b1d99283e8d01e8b9e8c2c4c6382a31817eaca
Kier Quebral <kierquebs@gmail.com>
2022-10-22 03:06:48 +0800

    created model that will be use on viewing transactions

--- model/TransactionModel.go
+++ model/TransactionModel.go
@@ -13,3 +13,20 @@ type NewTransactionModel struct {
 	PricePerKilo float64 `json:"price_per_kilo"`
 	MobileNo     string  `json:"mobile_no"`
 }
+
+type ViewTransactionModel struct {
+	RefID        string  `json:"ref_id"`
+	Trn_Date     string  `json:"trn_date"`
+	FirstName    string  `json:"first_name"`
+	LastName     string  `json:"last_name"`
+	MiddleName   string  `json:"middle_name"`
+	PricePerKilo float64 `json:"price_per_kilo"`
+	MobileNo     string  `json:"mobile_no"`
+	Pigs         struct {
+		ID             string  `json:"id"`
+		Added_Date     string  `json:"added_date"`
+		Qr_Code        string  `json:"qr_code"`
+		Final_Weight   float64 `json:"final_weight"`
+		Initial_Weight int     `json:"initial_weight"`
+	} `json:"pigs"`
+}
